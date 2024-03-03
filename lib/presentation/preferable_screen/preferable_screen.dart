d5407569157ce310f383617583e7f89afd7ab8ea
Damace <boniphace10@gmail.com>
2024-03-03 05:16:38 +0300

    first commit

--- lib/presentation/preferable_screen/preferable_screen.dart
+++ lib/presentation/preferable_screen/preferable_screen.dart
@@ -49,9 +49,7 @@ class _PreferableScreen extends State<PreferableScreen> {
         body: TabBarView(
           children: [
             sellerList(),
-            Center(
-              child: Text("Settings"),
-            ),
+            buyerList(),
           ],
         ),
       )),
@@ -61,7 +59,7 @@ class _PreferableScreen extends State<PreferableScreen> {
   sellerList() {
     final titles = ["Alex Mwakalikamo", "Seller B", "Seller C"];
     final subtitles = [
-      "Here is list 1 subtitle ffjdf dkffhkhkfhjkfjkhdjhfd fdkjfhdkjfhdjfddfd  eweqeqweqdewwrewrwerwerwer  djfkdfiudf",
+      "To comply with new evolving regulations, recently joined freelancers will need to fill in some essential account information before creating a Fiverr account. This helps ensure a trustworthy platform for everyone, fostering a strong foundation of trust between",
       "Here is list 2 subtitle",
       "Here is list 3 subtitle"
     ];
@@ -72,14 +70,21 @@ class _PreferableScreen extends State<PreferableScreen> {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+           
               height: MediaQuery.of(context).size.height *
                   0.15, // Set the height of the card container
               child: Card(
+                elevation: 5,
+               
                   child: Column(
                 children: [
                   ListTile(
                       onTap: () {},
-                      title: Text(titles[index],style: TextStyle(fontSize: 14,fontWeight:FontWeight.bold),),
+                      title: Text(
+                        titles[index],
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
                       subtitle: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
@@ -88,9 +93,16 @@ class _PreferableScreen extends State<PreferableScreen> {
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
-                      leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                              "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                      leading:
+                          // backgroundImage: NetworkImage(
+                          //     "https://images.unsplash.com/photo-1547721064-da6cfb341d50")
+
+                          Image(
+                        image: const AssetImage("assets/images/profile.png"),
+                        height: 50,
+                        width: 50,
+                        // fit: BoxFit.cover,
+                      ),
                       trailing: Visibility(
                         // if visibility is true, the child
                         // widget will show otherwise hide
@@ -101,8 +113,10 @@ class _PreferableScreen extends State<PreferableScreen> {
                         ),
                       )),
                   Padding(
-                    padding:
-                        const EdgeInsets.only(left: 35, right: 10,),
+                    padding: const EdgeInsets.only(
+                      left: 35,
+                      right: 10,
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
@@ -132,7 +146,10 @@ class _PreferableScreen extends State<PreferableScreen> {
                               }
                             }),
                         SizedBox(width: 5),
-                       Icon(Icons.share,color: Colors.grey,),
+                        Icon(
+                          Icons.share,
+                          color: Colors.grey,
+                        ),
                         SizedBox(width: 5),
                         Text("")
                       ],
@@ -145,6 +162,111 @@ class _PreferableScreen extends State<PreferableScreen> {
         });
   }
 
+  buyerList() {
+    final titles = ["Alex Mwakalikamo", "Seller B", "Seller C"];
+    final subtitles = [
+      "To comply with new evolving regulations, recently joined freelancers will need to fill in some essential account information before creating a Fiverr account. This helps ensure a trustworthy platform for everyone, fostering a strong foundation of trust between",
+      "Here is list 2 subtitle",
+      "Here is list 3 subtitle"
+    ];
+    final icons = [Icons.ac_unit, Icons.access_alarm, Icons.access_time];
+    return ListView.builder(
+        itemCount: titles.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+           
+              height: MediaQuery.of(context).size.height *
+                  0.15, // Set the height of the card container
+              child: Card(
+                elevation: 5,
+               
+                  child: Column(
+                children: [
+                  ListTile(
+                      onTap: () {},
+                      title: Text(
+                        titles[index],
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          subtitles[index],
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      leading:
+                          // backgroundImage: NetworkImage(
+                          //     "https://images.unsplash.com/photo-1547721064-da6cfb341d50")
+
+                          Image(
+                        image: const AssetImage("assets/images/profile.png"),
+                        height: 50,
+                        width: 50,
+                        // fit: BoxFit.cover,
+                      ),
+                      trailing: Visibility(
+                        // if visibility is true, the child
+                        // widget will show otherwise hide
+                        visible: true,
+                        child: Icon(
+                          Icons.verified_rounded,
+                          color: Colors.green,
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 35,
+                      right: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        LikeButton(
+                            likeBuilder: (bool isLiked) {
+                              return Icon(
+                                Icons.favorite,
+                                color: isLiked ? Colors.red : Colors.grey,
+                              );
+                            },
+                            likeCount: 0,
+                            countBuilder:
+                                (int? count, bool isLiked, String text) {
+                              var color = isLiked ? Colors.red : Colors.grey;
+                              Widget result;
+                              if (count == 0) {
+                                result = Text(
+                                  'Like',
+                                  style: TextStyle(color: Colors.red),
+                                );
+                              } else {
+                                result = Text(
+                                  text,
+                                  style: TextStyle(color: color),
+                                );
+                                return result;
+                              }
+                            }),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.share,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(width: 5),
+                        Text("")
+                      ],
+                    ),
+                  )
+                ],
+              )),
+            ),
+          );
+        });
+  }
   onTapBtnArrowleft() {
     Get.back();
   }
